04e1cac4441ef485ddf7812b3761a7132b8095eb
dok1959 <dok1959.good@mail.ru>
2021-03-07 22:42:17 +1000

    Создание личного кабинета (1 часть)
    Добавлены классы моделей для входа и регистрации пользователя, а также сама модель пользователя.
    
    Создан контроллер аккаунта и методы действий, для управлением входом и регистрацией.
    
    Добавлен класс для управлением ORM под названием ApplicationContext.

---
+++ MyLife/MyLife/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyLife.Models;
+using System.Security.Claims;
+
+namespace MyLife.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        ApplicationContext _context;
+        public AccountController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult Login([FromBody] LoginViewModel model)
+        {
+            var identity = GetIdentity(model.Login, model.Password);
+            if(identity == null)
+            {
+                return BadRequest(new { errorText = "Invalid login or password" });
+            }
+            return CreateJwtToken(identity);
+        }
+
+        [HttpPost]
+        [ActionName("register")]
+        public IActionResult Registration([FromBody] RegisterViewModel model)
+        {
+            return Ok();
+        }
+
+        [NonAction]
+        private ClaimsIdentity GetIdentity(string login, string password)
+        {
+            return null;
+        }
+
+        [NonAction]
+        private JsonResult CreateJwtToken(ClaimsIdentity identity)
+        {
+            return null;
+        }
+    }
+}
---
+++ MyLife/MyLife/Models/RegisterViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyLife.Models
+{
+    public class RegisterViewModel
+    {
+        public string Login { get; set; }
+
+        public string Email { get; set; }
+
+        public int Age { get; set; }
+
+        public string Password { get; set; }
+    }
+}
